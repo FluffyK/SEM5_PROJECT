9f7f7d45091ef7201f4ccc7a3ee2bd6e6fd5c3ab
FluffyK <andrei.lupascu@ati.utm.md>
2020-01-22 17:45:43 +0200

    add navbar components

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -14,7 +14,6 @@ import {
 
 function Header() {
   return (
-    <Router>
       <header class="header">
         <div>
           <div class="top_bar">
@@ -91,7 +90,7 @@ function Header() {
                                 </li>
                                 <li>
                                   <a class="clc">
-                                    <Link to="/category">Jewelry & Accessories</Link>
+                                    <Link to="/Category">Jewelry & Accessories</Link>
                                   </a>
                                 </li>
                                 <li>
@@ -165,110 +164,8 @@ function Header() {
               </div>
             </div>
           </div>
-
-          <nav class="main_nav">
-            <div class="container">
-              <div class="row">
-                <div class="col">
-                  <div class="main_nav_content d-flex flex-row">
-                    <div class="cat_menu_container">
-                      <div class="cat_menu_title d-flex flex-row align-items-center justify-content-start">
-                        <div class="cat_burger">
-                          <span></span>
-                          <span></span>
-                          <span></span>
-                        </div>
-                        <div class="cat_menu_text">Categories</div>
-                      </div>
-                      <ul class="cat_menu">
-                        <li><a>
-                            <Link to="/category">Jewelry & Accessories{" "}</Link>
-                            <i class="fas fa-chevron-right ml-auto"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Shoes<i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Clothing<i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Entertainment<i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Vintage<i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Toys<i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="category.html">
-                            Produces for Home
-                            <i class="fas fa-chevron-right"></i>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-
-                    <div class="main_nav_menu ml-auto">
-                      <ul class="standard_dropdown main_nav_dropdown">
-                        <li>
-                          <a><Link to="/">Home</Link>
-                          </a>
-                        </li>
-                        <li class="hassubs">
-                          <a href="category.html">
-                            All Categories<i class="fas fa-chevron-down"></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a href="contact.html">
-                            Contact Us<i class="fas fa-chevron-down"></i>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="menu_trigger_container ml-auto">
-                      <div class="menu_trigger d-flex flex-row align-items-center justify-content-end">
-                        <div class="menu_burger">
-                          <div class="menu_trigger_text">menu</div>
-                          <div class="cat_burger menu_burger_inner">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </nav>
-          <Switch>
-            <Route path="/category">
-              <Category />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-            {/* <Route path="/category">
-            <Category />
-            </Route> */}
-          </Switch>
         </div>
       </header>
-    </Router>
   );
 }
 export default Header;
---
+++ src/components/NavBars/CatNavBar/Cat_nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Cat_nav() {
+  return (
+    <div>
+      <section class="breadcrumb breadcrumb_bg">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-12">
+              <div class="breadcrumb_iner">
+                <div class="breadcrumb_iner_item">
+                  <p>Home / All Categories</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
