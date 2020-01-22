398c510e431fdb0eb6909f0f234f0e1b98eab7f8
FluffyK <andrei.lupascu@ati.utm.md>
2020-01-22 17:00:58 +0200

    Home route

---
+++ src/components/Category/category.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Header/header'
+import Footer from '../Footer/footer'
+
+export default function category() {
+    return (
+        <div>
+            <Header></Header>
+            <p>mata ii curva</p>
+            <Footer></Footer>
+        </div>
+    )
+}
