fd6f1db123c57881099d25a1e412a1d54d14bf90
FluffyK <andrei.lupascu@ati.utm.md>
2020-01-23 11:18:52 +0200

    merge categories

--- src/components/Category/category.js
+++ src/components/Category/category.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 // import Header from '../Header/header'
 // import Footer from '../Footer/footer'
@@ -10,4 +11,134 @@ export default function Category() {
             {/* <Footer></Footer> */}
         </div>
     )
+=======
+import React from "react";
+
+export default function Category() {
+  return (
+    <div className="App">
+      <section class="cat_product_area section_padding border_top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3">
+              <div class="left_sidebar_area">
+                <aside class="left_widgets p_filter_widgets sidebar_box_shadow">
+                  <div class="l_w_title">
+                    <h3>All Categories</h3>
+                  </div>
+                  <div class="widgets_inner">
+                    <ul class="list">
+                      <li>
+                        <a>Jewelry & Accessories</a>
+                      </li>
+
+                      <li>
+                        <a>Clothing</a>
+                      </li>
+                      <li>
+                        <a>Shoes</a>
+                      </li>
+                      <li>
+                        <a>Entertainment</a>
+                      </li>
+                      <li>
+                        <a>Toys</a>
+                      </li>
+                      <li>
+                        <a>Vintage</a>
+                      </li>
+                      <li>
+                        <a>Produces for Home</a>
+                      </li>
+                    </ul>
+                  </div>
+                </aside>
+                <aside class="left_widgets p_filter_widgets sidebar_box_shadow">
+                  <div class="l_w_title">
+                    <h3>General filters</h3>
+                  </div>
+                  <div class="widgets_inner">
+                    <ul class="list">
+                      <p></p>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Women</a>
+                      </li>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Men</a>
+                      </li>
+                      <li class="active">
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Kids</a>
+                      </li>
+                    </ul>
+                    <ul class="list">
+                      <p>Color</p>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Black</a>
+                      </li>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Pink</a>
+                      </li>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Red</a>
+                      </li>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Gold</a>
+                      </li>
+                      <li>
+                        <input
+                          type="radio"
+                          aria-label="Radio button for following text input"
+                        />
+                        <a>Spacegrey</a>
+                      </li>
+                    </ul>
+                  </div>
+                </aside>
+              </div>
+            </div>
+            <div class="col-lg-9">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="product_top_bar d-flex justify-content-between align-items-center">
+                    <div class="single_product_menu product_bar_item">
+                      <h4>Women (8)</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
---
+++ src/components/Product/CategoryProduct.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function CategoryProduct() {
+    return (
+        <div className="App">
+            <div class="col-lg-4 col-sm-6">
+<div class="single_category_product">
+<div class="single_category_img">
+<img src="2.jpg" alt="">
+<div class="category_social_icon">
+<ul>
+<li><a href="#"><i class="fa fa-heart-o"></i></a></li>
+<li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+</ul>
+</div>
+<div class="category_product_text">
+<a href="details.html"><h5>Jewelry</h5></a>
+<p>$5.00</p>
+</div>
+</div>
+</div>
+</div>            
+        </div>
+    )
+}
